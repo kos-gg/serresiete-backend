6c6827a27749181492d379b79796547d2e59ae70
Sancho, Xavier <xavier.sanchoruiz@sky.uk>
2026-04-07 11:12:09 +0100

    * sanxei95 - robocop merge request

---
+++ src/main/resources/db/migration/prod/V1774828801__Roles_Activities_Values.sql
@@ -0,0 +1,4 @@
+insert into activities values('get wow run details');
+
+insert into roles_activities values('admin', 'get wow run details');
+insert into roles_activities values('service', 'get wow run details');
